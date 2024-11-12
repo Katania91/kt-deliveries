902374d9861c7cf9a5c620a4f45ffa017ec6395b
Kevin <163057317+Katania91@users.noreply.github.com>
2024-11-12 20:33:59 +0100

    Version 1.1.0

--- locales/fr.lua
+++ locales/fr.lua
@@ -11,10 +11,10 @@ Locales['fr'] = {
     no_more_packages = "Vous avez terminé toutes les livraisons ! Plus de colis à livrer.",
 
     -- Messages de progression
-    start_job = "Bonne chance ! Je vous prête des vêtements adaptés.",
+    start_job = "Bon travail ! Je te prête une tenue appropriée. Je prends 300$ comme caution pour le fourgon, que tu récupéreras à ton retour.",
     load_package = "Livraison du colis...",
     go_to_next_delivery = "Dirigez-vous vers la prochaine livraison.",
-    job_finished = "Vous avez terminé le travail. Voici vos vêtements.",
+    job_finished = "Tu as terminé le travail. Voici tes vêtements et les 300$ de caution.",
 
     -- Étiquettes pour les interactions ox_target
     label_talk_michael = "Parler à Michael",
