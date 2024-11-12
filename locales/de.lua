c30c516e6caa80fb88e7dab5a4bb1848e910e209
Kevin <163057317+Katania91@users.noreply.github.com>
2024-11-12 20:33:06 +0100

    Version 1.1.0

--- locales/de.lua
+++ locales/de.lua
@@ -11,10 +11,10 @@ Locales['de'] = {
     no_more_packages = "Alle Lieferungen abgeschlossen! Keine Pakete mehr übrig.",
 
     -- Fortschrittsnachrichten
-    start_job = "Viel Glück bei der Arbeit! Ich leihe dir passende Kleidung.",
+    start_job = "Gute Arbeit wünsche ich dir! Ich leihe dir passende Kleidung. Ich nehme 300$ als Kaution für den Lieferwagen, die du bei deiner Rückkehr zurückbekommst.",
     load_package = "Paket wird geliefert...",
     go_to_next_delivery = "Gehe zur nächsten Lieferung.",
-    job_finished = "Arbeit beendet. Hier hast du deine Kleidung zurück.",
+    job_finished = "Du hast die Arbeit beendet. Hier sind deine Kleidung und die 300$ Kaution zurück.",
 
     -- ox_target-Interaktionsbeschriftungen
     label_talk_michael = "Mit Michael sprechen",
