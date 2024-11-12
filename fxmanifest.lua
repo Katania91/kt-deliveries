8483ede6d28ada383356915ec6cdd7a36fc4d44b
Kevin <163057317+Katania91@users.noreply.github.com>
2024-11-12 20:32:15 +0100

    Version 1.1.0
    Added new functions and compatibility to QBox, ND and OX core.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,8 @@ game 'gta5'
 lua54 'yes'
 
 author 'Katania'
-description 'Script per consegna pacchi multilingua'
-version '1.0.0'
+description 'kt-deliveries: Delivery Job Script for FiveM multilanguage'
+version '1.1.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -23,5 +23,9 @@ server_scripts {
 dependencies {
     'ox_lib',
     'ox_target',
-    'qb-core'
+    'qb-core',      -- QBCore support (required if using QBCore framework)
+    -- 'es_extended', -- Uncomment if using ESX framework
+    -- 'ox_core',     -- Uncomment if using OX Core framework
+    -- 'qbox-core',   -- Uncomment if using Qbox framework
+    -- 'nd-core'      -- Uncomment if using ND Core framework
 }
